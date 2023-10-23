dc5c8424b5d4f8f8e32ab55d73bc1bfbd71878f8
Martin Kontsek <martin.kontsek@fri.uniza.sk>
2023-10-23 11:38:25 +0200

    cv03: Chat client dokonceny s vlaknami.

--- cv03/cv03-tcp-klient.py
+++ cv03/cv03-tcp-klient.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import socket as s
+from threading import Lock
+
+MSG_SIZE = 100
 
 class ChatProtocol:
     def __init__(self, nick):
@@ -15,19 +18,49 @@ class ChatProtocol:
     def send_msg(self, msg):
         return "SENDMSG|{}|{}".format(self._nick, msg).encode()
     
-    def parse(self, bin_msg, user_list):
+    def who(self):
+        return "WHO|{}".format(self._nick).encode()
+    
+    def users(self, user_list):
+        users = ""
+        for user in user_list:
+            users += user + ","
+        if len(users) > 1:
+            users = users[0:len(users)-1]
+        return "USERS|{}".format(users).encode()
+    
+    def parse(self, bin_msg : bytes, user_list : list, client_sock : s.socket, lock : Lock):
         str_msg = bin_msg.decode()
         list_msg_parts = str_msg.split("|")
+        if len(list_msg_parts) > 1:
+            nick = list_msg_parts[1]
+        if len(list_msg_parts) > 2:
+            message = list_msg_parts[2]
 
         if list_msg_parts[0] == "LOGIN":
-            user_list.append(list_msg_parts[1])
+            lock.acquire()
+            user_list.append(nick)
+            lock.release()
+            print("Client '{}' has been connected.".format(nick))
         
         elif list_msg_parts[0] == "EXIT":
-            user_list.remove(list_msg_parts[1])
+            lock.acquire()
+            user_list.remove(nick)
+            lock.release()
+            print("Client '{}' has been disconnected.".format(nick))
 
         elif list_msg_parts[0] == "SENDMSG":
-            print("Client '{}' msg: {}".format(list_msg_parts[1],
-                                                list_msg_parts[2]))
+            print("Client '{}' msg: {}".format(nick,
+                                                message))
+            
+        elif list_msg_parts[0] == "WHO":
+            print("Client '{}' requested list of users.".format(nick))
+            client_sock.send(self.users(user_list))
+
+        elif list_msg_parts[0] == "USERS":
+            users = nick.split(",")
+            print("Logged in users: {}.".format(users))
+
 
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 9999
@@ -47,6 +80,10 @@ while True:
             sock.send(protocol.exit())
             sock.close()
             exit(0)
+        elif msg[1] == "w":
+            sock.send(protocol.who())
+            bin_msg = sock.recv(MSG_SIZE)
+            protocol.parse(bin_msg, None, None, None)
 
     sock.send(protocol.send_msg(msg))
 
--- cv03/cv03-tcp-server.py
+++ cv03/cv03-tcp-server.py
@@ -6,11 +6,14 @@
 # OPERACIE: LOGIN|nick
 #           SENDMSG|nick|Ahoj ako sa mas (sprava nemoze obsahovat|)
 #           EXIT|nick
+#           WHO|nick                    # klient pyta zoznam prihlasenych pouzivatelov
+#           USERS|nick1,nick2,nick3...  # server posiela zoznam pouzivatelov ako odpoved na WHO
 
 MSG_SIZE = 100
 USERS = list()
 
 import socket as s
+from threading import Thread, Lock
 
 class ChatProtocol:
     def __init__(self, nick):
@@ -25,19 +28,57 @@ class ChatProtocol:
     def send_msg(self, msg):
         return "SENDMSG|{}|{}".format(self._nick, msg).encode()
     
-    def parse(self, bin_msg, user_list):
+    def who(self):
+        return "WHO|{}".format(self._nick).encode()
+    
+    def users(self, user_list):
+        users = ""
+        for user in user_list:
+            users += user + ","
+        if len(users) > 1:
+            users = users[0:len(users)-1]
+        return "USERS|{}".format(users).encode()
+    
+    def parse(self, bin_msg : bytes, user_list : list, client_sock : s.socket, lock : Lock):
         str_msg = bin_msg.decode()
         list_msg_parts = str_msg.split("|")
+        if len(list_msg_parts) > 1:
+            nick = list_msg_parts[1]
+        if len(list_msg_parts) > 2:
+            message = list_msg_parts[2]
 
         if list_msg_parts[0] == "LOGIN":
-            user_list.append(list_msg_parts[1])
+            lock.acquire()
+            user_list.append(nick)
+            lock.release()
+            print("Client '{}' has been connected.".format(nick))
         
         elif list_msg_parts[0] == "EXIT":
-            user_list.remove(list_msg_parts[1])
+            lock.acquire()
+            user_list.remove(nick)
+            lock.release()
+            print("Client '{}' has been disconnected.".format(nick))
 
         elif list_msg_parts[0] == "SENDMSG":
-            print("Client '{}' msg: {}".format(list_msg_parts[1],
-                                                list_msg_parts[2]))
+            print("Client '{}' msg: {}".format(nick,
+                                                message))
+            
+        elif list_msg_parts[0] == "WHO":
+            print("Client '{}' requested list of users.".format(nick))
+            client_sock.send(self.users(user_list))
+
+        elif list_msg_parts[0] == "USERS":
+            users = nick.split(",")
+            print("Logged in users: {}.".format(users))
+
+
+def handle_client(client_sock, lock):
+    protocol = ChatProtocol("")
+    while True:
+        client_msg = client_sock.recv(MSG_SIZE)
+        protocol.parse(client_msg, USERS, client_sock, lock)
+        
+    client_sock.close()
 
 
 sock = s.socket(family=s.AF_INET, type=s.SOCK_STREAM)
@@ -48,17 +89,13 @@ sock.listen(10)
 # client_sock = navrat[0]
 # client_addr = navrat[1]
 
-protocol = ChatProtocol("")
+lock = Lock()
 
 while True:
     (client_sock, client_addr) = sock.accept()
-    print("Connected client: ({0}:{1}).".format(client_addr[0],client_addr[1]))
+    print("Connected client TCP session created: ({0}:{1}).".format(client_addr[0],client_addr[1]))
+    t = Thread(target=handle_client, args=[client_sock, lock])
+    t.run()
 
-    while True:
-        client_msg = client_sock.recv(MSG_SIZE)
-        protocol.parse(client_msg, USERS)
-        
-    client_sock.close()
 
 sock.close()
-
